27966f9d27cf10da785be3b41812de8ac800c4ff
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-06-29 14:13:42 +0900

    created bubble sort　and insertion sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,9 +1,13 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        Integer[] numList = {8,7,6,5,4,3,2,1};
+        Integer[] numList = {4,6,2,8,5,12,1,8};
+        //accending sort
         Arrays.sort(numList);
+        //descending sort
+        //Arrays.sort(numList,Comparator.reverseOrder());
         for(int i : numList){
             System.out.println(i);
         }
---
+++ InsertionSort.java
@@ -0,0 +1,28 @@
+public class InsertionSort {
+    static void insertionSort(int data[], int arraySize){
+        int j, temp;
+        for(int i = 1; i < arraySize; i++) {
+            j = i;
+
+            //descending order
+            //while(i>0 && data[j-1]<data[i])
+            //accending order
+            while (j > 0 && data[j - 1] > data[j]) {
+                temp = data[j - 1];
+                data[j - 1] = data[j];
+                data[j] = temp;
+                j -= 1;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] data = {6,7,8,91,4,3};
+        int arraySize = data.length;
+        insertionSort(data, arraySize);
+
+        for(int i = 0; i < arraySize; i++){
+            System.out.print(data[i]+" ");
+        }
+
+    }
+}
