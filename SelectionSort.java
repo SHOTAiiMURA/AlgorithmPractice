7cfb06e28bb338f05ad451c4084063e9b70e997c
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-07-06 10:50:27 +0900

    learn selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort {
+    //quadratic time O(n2)
+    //small data set = good
+    //large data set = bad
+    public static void main(String[] args) {
+        int[] arr = {8,2,3,6,9,1,4,7,5,1};
+        selectionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(i + " ");
+        }
+    }
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
